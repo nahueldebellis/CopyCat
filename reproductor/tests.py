e900db92b50565d5dc744b902b876b870ddd8515
nahueldebellis <debellisnahuel@gmail.com>
2019-06-03 01:30:07 -0300

    create test for player upload and add a link to see how read a file from a request

--- reproductor/tests.py
+++ reproductor/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from wave
+from io import BytesIO
 # Create your tests here.
+
+class Player(TestCase):
+    def setUp(self):
+        self.song_name = 'europa.wav' # setup
+        self.destination = '%s' % self.song_name # setup
+        self.client = Client() # setup
+        self.chunk = 1024 # setup
+        self.binary_song = b'' # setup
+
+    def upload(self):
+        with open(self.destination, 'rb') as data: # if doesn't work then use wave.open to read binary data
+            byte = data.read(self.chunk)
+            while byte:
+                self.binary_song = binary+byte
+                byte = f.read(self.chunk)
+
+            song = BytesIO(self.binary_song)
+            song.name = self.song_name
+
+            response = self.client.post('/player/upload/', {'song': song}, follow=True)
+            self.assertEqual(response.status_code, 201)
+
+
+
--- reproductor/views.py
+++ reproductor/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# example from django docs for read a request file 
+# https://docs.djangoproject.com/en/2.2/topics/http/file-uploads/
