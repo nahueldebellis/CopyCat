06d668fc4e52d3484bcb5d830fd529fcccd2a851
nahuel <debellisnahuel@gmail.com>
2019-07-06 19:10:52 -0300

    update react server, error dependencias

--- design/copycat/src/index.js
+++ design/copycat/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Register from './Register/Register';
+import Player from './Reproductor/Reproductor';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Register />, document.getElementById('root'));
+ReactDOM.render(<Player />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
